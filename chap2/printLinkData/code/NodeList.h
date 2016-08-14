3740c3492b9bd5062d592c6b1d641988369da86d
zk <zk@zk.com>
2016-08-15 06:52:48 +0800

    完成链表的反向打印

--- chap2/printLinkData/code/NodeList.h
+++ chap2/printLinkData/code/NodeList.h
@@ -1,6 +1,8 @@
 #ifndef H3296D1EE_ADA3_43C1_B40B_0EDC7E15FBDA
 #define H3296D1EE_ADA3_43C1_B40B_0EDC7E15FBDA
 
+#include <stack>
+
 template<typename T>
 struct Node
 {
@@ -20,7 +22,8 @@ struct NodeList
 		return _size;
 	}
 
-	void print();
+	void print() const;
+	void antiPrint() const;
 private:
 private:
 	NodeType *head;
@@ -29,7 +32,8 @@ private:
 };
 
 template<typename T>
-NodeList<T>::NodeList() : head(NULL), tail(NULL), _size(0)
+NodeList<T>::NodeList()
+		: head(NULL), tail(NULL), _size(0)
 {
 
 }
@@ -41,27 +45,29 @@ void NodeList<T>::add(T val)
 	pNew->val = val;
 	pNew->next = NULL;
 
-	if(head == NULL)
+	if (head == NULL)
 	{
 		head = pNew;
 	}
 
-	if(tail != NULL)
+	if (tail != NULL)
 	{
 		tail->next = pNew;
 	}
 	tail = pNew;
 
 	_size++;
+
+	print();
+	antiPrint();
 }
 
 template<typename T>
 void NodeList<T>::del(T value)
 {
-	if(head == NULL)
-		return;
+	if (head == NULL) return;
 
-	if(head->val == value)
+	if (head->val == value)
 	{
 		NodeType *pTmp = head;
 		head = head->next;
@@ -70,11 +76,13 @@ void NodeList<T>::del(T value)
 		_size--;
 	}
 
+	if (head == NULL) return;
+
 	NodeType *pCur = head->next;
 	NodeType *pPre = head;
-	while(pCur != NULL)
+	while (pCur != NULL)
 	{
-		if(pCur->val == value)
+		if (pCur->val == value)
 		{
 			pPre->next = pCur->next;
 			delete pCur;
@@ -87,17 +95,41 @@ void NodeList<T>::del(T value)
 		pPre = pCur;
 		pCur = pCur->next;
 	}
+
+	print();
+	antiPrint();
 }
 
 template<typename T>
-void NodeList<T>::print()
+void NodeList<T>::print() const
 {
 	std::cout << "size: " << _size << std::endl;
 	NodeType *node = head;
-	while(node != NULL)
+	while (node != NULL)
 	{
 		std::cout << node->val << " -> ";
 		node = node->next;
+	}
+
+	std::cout << std::endl;
+}
+
+template<typename T>
+void NodeList<T>::antiPrint() const
+{
+	std::stack<T> nodeStack;
+
+	NodeType *cur = head;
+	while(cur != NULL)
+	{
+		nodeStack.push(cur->val);
+		cur = cur->next;
+	}
+
+	while(!nodeStack.empty())
+	{
+		std::cout << nodeStack.top() << " -> " ;
+		nodeStack.pop();
 	}
 
 	std::cout << std::endl;
--- chap2/printLinkData/testcase/testcase.cpp
+++ chap2/printLinkData/testcase/testcase.cpp
@@ -4,13 +4,17 @@
 
 TEST_F(TestLinkList, check_add)
 {
-	nodeList.print();
 	nodeList.del(1);
-	nodeList.print();
+	nodeList.antiPrint();
 	nodeList.add(1);
 	nodeList.add(2);
-	nodeList.print();
 	nodeList.del(2);
-	nodeList.print();
+	nodeList.del(2);
+	nodeList.del(10);
+	nodeList.del(1);
+	nodeList.del(3);
+	nodeList.del(4);
+	nodeList.del(5);
+	nodeList.del(1);
 }
 
