d7e991a2148cea03d808db755afc085e4599d36c
zk <zk@zk.com>
2016-08-17 06:55:01 +0800

    完成2个stack实现一个queue的一半半

---
+++ chap2/ImplQueueWithStack/code/MyQueue.h
@@ -0,0 +1,44 @@
+#ifndef HFF962980_0BBF_4D56_938C_18954CF44E36
+#define HFF962980_0BBF_4D56_938C_18954CF44E36
+
+#include <stack>
+
+using std::stack;
+
+template<typename T>
+struct MyQueue
+{
+	typedef stack<T> StackType;
+
+	void push(const T& val);
+	T& front();
+	void pop();
+
+private:
+	void move(StackType &srcStack, StackType &dstStack);
+private:
+	StackType firstStack;
+	StackType secondStack;
+};
+
+template<typename T>
+void MyQueue<T>::push(const T& val)
+{
+	if(firstStack.empty())
+		move(secondStack, firstStack);
+
+	firstStack.push(val);
+}
+
+template<typename T>
+void MyQueue<T>::move(StackType &srcStack, StackType &dstStack)
+{
+	while(!srcStack.empty())
+	{
+		dstStack.push(srcStack.top());
+		srcStack.pop();
+	}
+}
+
+
+#endif /* HFF962980_0BBF_4D56_938C_18954CF44E36 */
